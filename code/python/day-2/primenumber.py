82156b40cdeab4d5b155a7b43383ffeed2393c7a
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-07-26 23:54:17 +0530

    update

---
+++ code/python/day-2/primenumber.py
@@ -0,0 +1,13 @@
+'''Program to find that the number is prime or not'''
+number=int(input("enter the number"))
+flag=0
+for i in range(2,number):
+    
+    if(number%i==0):
+        flag=1
+        break
+if(flag==0):
+    print("number",number,"is prime")
+else:
+    print("number",number,"is  not prime")
+              
