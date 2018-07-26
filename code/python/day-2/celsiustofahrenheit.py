73a5488456bd1fe497fb6f22d0236d79e3801ea8
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-07-27 00:25:51 +0530

    update repository

---
+++ code/python/day-2/celsiustofahrenheit.py
@@ -0,0 +1,4 @@
+'''program to convert celsius to fahrenheit'''
+celsius=float(input("enter the temprature in celsius"))
+fahrenheit=((9/5)*celsius+32)
+print("temperature in",celsius,"celsius  to fahrenheit is",fahrenheit) 
---
+++ code/python/day-2/reverse_a_string_usingslice.py
@@ -0,0 +1,6 @@
+'''program to reverse a string'''
+
+string=str(input("enter a string:"))
+print(string[::-1])
+
+
