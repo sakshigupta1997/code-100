dfffc7de5b6ec09b1d8c96b731a753acac03edec
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-07-27 23:48:46 +0530

    update repository

---
+++ code/python/day-3/evenodd.py
@@ -0,0 +1,6 @@
+'''program to find a number is even or odd'''
+number=int(input("enter a number:"))
+if(number%2==0):
+    print("Number",number," is even")
+else:
+    print("Number",number," is odd")
