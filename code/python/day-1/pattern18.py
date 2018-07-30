15112794df233f222b5e5f2837126860d07725a3
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-07-30 23:53:28 +0530

    update repository

---
+++ code/python/day-1/pattern18.py
@@ -0,0 +1,37 @@
+'''program to pattern of diamond but without taking an input from user
+using five loop
+      *
+     ***
+    *****
+   *******
+  *********
+ ***********
+ ***********
+  *********
+   *******
+    *****
+     ***
+      *    '''
+   
+p=6
+m=5
+y=1
+for i in range(0,12):
+    if(i<=5):
+        
+        for j in range(p,0,-1):
+            print(" ",end='')
+        for k in range(2*i+1):
+            print("*",end='')
+    if(i>5):
+        for j in range(y):
+            print(" ",end='')
+        y=y+1
+        for k in range(2*m+1):
+            print("*",end='')
+        m=m-1
+    print()
+    p=p-1
+    
+    
+    
