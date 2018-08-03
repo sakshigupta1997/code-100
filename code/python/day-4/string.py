2e8d7192379f71690c5e5b7b8f7f028b89c58396
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-08-03 23:54:50 +0530

    update repository

---
+++ code/python/day-4/string.py
@@ -0,0 +1,13 @@
+string='sakshigupta'
+
+print(string.find('shi'))#its show the substring that you specify and its starting location is 3
+print(string.replace('i','a'))
+
+print(string.split(,',',2))
+print(string.count('s'))
+print(string.upper())
+print(max(string))
+print(min(string))
+print(string.isalpha())
+string2='sakshi,gupta'
+print(string2.isalpha())#because of comma it show there is no alphabet present
---
+++ code/python/day-6/equation_solving.py
@@ -0,0 +1,4 @@
+x=float(input("enter the value of x:"))
+y=(pow(x,3)-(3*pow(x,2))+(2*x)+9)
+
+print("value of y is",float(y),"for x equal to",x)
