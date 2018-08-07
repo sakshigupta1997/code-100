5a2f6f9f2587c57fc0347e6a3be9b3b0912181ae
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-08-07 22:29:23 +0530

    linear_search

--- code/python/day-7/Linear_searchh.py
+++ code/python/day-7/Linear_searchh.py
@@ -1,13 +1,13 @@
-
+'''program to search an element in a list'''
 number=int(input("enter the numbers"))
 y=[]
 flag=0
 for i in range(0,number):
     y.append(i)
 print(y)
-item=int(input("enter the item you want to insert"))
+item=int(input("enter the item you want to search"))
 for i in range(0,number):
-    if(item==y.index(i)):
+    if(item==y[i]):
         flag=1
         break
     else:
