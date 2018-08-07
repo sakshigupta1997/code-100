5006f90292315cfe288c1e2c431651fb2c44529e
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-08-07 22:54:56 +0530

    binary_search

---
+++ code/python/day-7/binary_search.py
@@ -0,0 +1,24 @@
+number=int(input("enter the number"))
+y=[]
+flag=0
+for i in range(0,number):
+    y.append(i)
+print(y)
+beg=0
+end=number-1
+item=int(input("enter the item you want to searched"))
+while(beg<=end):
+    
+    mid=int((beg+end)/2)
+    if(item==y[mid]):
+        flag=1
+        break
+    else:
+        if(item<y[mid]):
+            end=mid-1
+        else:
+            beg=mid+1
+if(flag==0):
+    print("item not found")
+else:
+    print("item found")
