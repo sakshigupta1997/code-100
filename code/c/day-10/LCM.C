a4e216597de3fcd6a73312227480bc20b14bb601
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-08-15 22:31:05 +0530

    lcm_of_two_number

---
+++ code/c/day-10/LCM.C
@@ -0,0 +1,35 @@
+/*program to find the lcm of two number*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int number1,number2,i=2,p=1;
+clrscr();
+printf("enter the two number");
+scanf("%d%d",&number1,&number2);
+while(number1!=1||number2!=1)
+{
+
+while(number1%i==0||number2%i==0)
+{
+if(number1%i==0&&number2%i==0)
+{
+number1=number1/i;
+number2=number2/i;
+}
+else if(number1%i==0)
+{
+number1=number1/i;
+}
+else
+{
+number2=number2/i;
+
+}
+p=p*i;
+}
+i++;
+}
+printf("lcm of two number is %d",p);
+getch();
+}
