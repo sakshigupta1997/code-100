b8e17b6b3f66f66747ce2ed0551dd31dd7461631
sakshigupta1997 <40806036+sakshigupta1997@users.noreply.github.com>
2018-08-19 23:50:17 +0530

    sum_of_digits_of number

---
+++ code/c/challenges/SUM_OF_D.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,p,rem,sum=0;
+clrscr();
+printf("enter a number");
+scanf("%d",&n);
+p=n;
+while(n!=0)
+{
+rem=n%10;
+sum=sum+rem;
+n=n/10;
+}
+printf("sum of digit of number %d is %d",p,sum);
+getch();
+}
