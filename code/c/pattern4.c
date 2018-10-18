884c6ff4a8e4da15a72a068f8ec8110c96dadc13
Sakshi Gupta <40806036+sakshigupta1997@users.noreply.github.com>
2018-10-19 01:28:00 +0530

    Update pattern4.c

--- code/c/pattern4.c
+++ code/c/pattern4.c
@@ -2,5 +2,20 @@
 #include<conio.h>
 void main()
 {
+  int i,j,k,l,m=0,l=0;
+  for(i=0;i<5;i++)
+  {
+    for(j=m;j<5;j++)
+    {
+      printf("*");
+    }
+    for(k=0;k!=i;k++)
+    {
+      printf("%d",l);
+      l++;
+    }
+    m++;
+    printf("\n");
+  }
 
 }
