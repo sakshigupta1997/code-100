de9b72a5c89fa4fdc369166b15000a285764d8b9
Sakshi Gupta <40806036+sakshigupta1997@users.noreply.github.com>
2019-01-04 23:49:44 +0530

    string.py

--- code/python/day-4/string.py
+++ code/python/day-4/string.py
@@ -3,7 +3,7 @@ string='sakshigupta'
 print(string.find('shi'))#its show the substring that you specify and its starting location is 3
 print(string.replace('i','a'))
 
-print(string.split(,',',2))
+print(string.split(,',',2))  
 print(string.count('s'))
 print(string.upper())
 print(max(string))
